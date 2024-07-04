50bba3898dd3c8ef5165886b7014ad1734f0bb0b
Philip-11 <magsaysayphil@gmail.com>
2024-07-04 11:44:09 +0800

    added registration of users

--- users.php
+++ users.php
@@ -1,3 +1,26 @@
+<?php 
+
+include 'connect.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
+    $ulevel = $_POST["user_level"];
+    $fname = $_POST["fname"];
+    $lname = $_POST["lname"];
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+
+    $sql = "INSERT INTO users (user_level, fname, lname, password, email) values ('1', '$fname', '$lname', '$password', '$email')";
+
+    if (mysqli_query($con, $sql)){
+        echo "Registered Successfully!";
+    } else {
+        echo "Error" . $sql . mysqli_error($con);
+    }
+}
+
+
+?>
+
 <!DOCTYPE html>
 <html>
 
@@ -22,7 +45,7 @@
                 <div class="alert alert-info absolue center" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span></button><span>Profile save with success</span></div>
             </div>
         </div>
-        <form>
+        <form method="post" action="">
             <div class="form-row profile-row">
                 <div class="col-md-4 relative">
                     <div class="avatar">
@@ -33,16 +56,16 @@
                     <hr>
                     <div class="form-row">
                         <div class="col-sm-12 col-md-6">
-                            <div class="form-group"><label>Firstname </label><input class="form-control" type="text" name="firstname"></div>
+                            <div class="form-group"><label>Firstname </label><input class="form-control" type="text" name="fname" id="fname"></div>
                         </div>
                         <div class="col-sm-12 col-md-6">
-                            <div class="form-group"><label>Lastname </label><input class="form-control" type="text" name="lastname"></div>
+                            <div class="form-group"><label>Lastname </label><input class="form-control" type="text" name="lname" id="lname"></div>
                         </div>
                     </div>
-                    <div class="form-group"><label>Email </label><input class="form-control" type="email" autocomplete="off" required="" name="email"></div>
+                    <div class="form-group"><label>Email </label><input class="form-control" type="email" autocomplete="off" required="" name="email" id="email"></div>
                     <div class="form-row">
                         <div class="col-sm-12 col-md-6">
-                            <div class="form-group"><label>Password </label><input class="form-control" type="password" name="password" autocomplete="off" required=""></div>
+                            <div class="form-group"><label>Password </label><input class="form-control" type="password" name="password" id="password" autocomplete="off" required=""></div>
                         </div>
                         <div class="col-sm-12 col-md-6">
                             <div class="form-group"><label>Confirm Password</label><input class="form-control" type="password" name="confirmpass" autocomplete="off" required=""></div>
@@ -50,7 +73,7 @@
                     </div>
                     <hr>
                     <div class="form-row">
-                        <div class="col-md-12 content-right"><button class="btn btn-primary form-btn" type="submit">SAVE </button><button class="btn btn-danger form-btn" type="reset">CANCEL </button></div>
+                        <div class="col-md-12 content-right"><button class="btn btn-primary form-btn" type="submit" value="Register">SAVE </button><button class="btn btn-danger form-btn" type="reset">CANCEL </button></div>
                     </div>
                 </div>
             </div>
@@ -58,7 +81,7 @@
     </div>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/js/bootstrap.bundle.min.js"></script>
-    <script src="assets/js/Profile-Edit-Form.js"></script>
+    <!-- <script src="assets/js/Profile-Edit-Form.js"></script> -->
 </body>
 
 </html>
